e4b87a9ad62c645bd11fcc75a4915e046b43a375
Jessika Penttinen <jessika.penttinen@siili.com>
2018-09-18 14:30:04 +0300

    Healthcheck-api

--- src/main/scala/fi/oph/kouta/ScalatraBootstrap.scala
+++ src/main/scala/fi/oph/kouta/ScalatraBootstrap.scala
@@ -1,7 +1,7 @@
 import fi.oph.kouta.config.{KoutaConfiguration, KoutaConfigurationFactory}
 import fi.oph.kouta.repository.KoutaDatabase
 import fi.oph.kouta.{KoutaBackendSwagger, SwaggerServlet}
-import fi.oph.kouta.servlet.KoulutusServlet
+import fi.oph.kouta.servlet.{HealthcheckServlet, KoulutusServlet}
 import fi.vm.sade.utils.slf4j.Logging
 import org.scalatra._
 import javax.servlet.ServletContext
@@ -16,10 +16,10 @@ class ScalatraBootstrap extends LifeCycle with Logging {
 
     implicit val swagger = new KoutaBackendSwagger
 
+    context.mount(new HealthcheckServlet(), "/healthcheck", "healthcheck")
     context.mount(new KoulutusServlet(), "/komo", "komo")
 
     context.mount(new SwaggerServlet, "/swagger")
-
   }
 
   override def destroy(context: ServletContext) = {
--- src/main/scala/fi/oph/kouta/servlet/koutaServlet.scala
+++ src/main/scala/fi/oph/kouta/servlet/koutaServlet.scala
@@ -3,12 +3,27 @@ package fi.oph.kouta.servlet
 import fi.oph.kouta.domain.{Julkaisutila, Koulutustyyppi}
 import org.json4s.{DefaultFormats, Formats}
 import org.json4s.ext.EnumNameSerializer
-import org.scalatra.ScalatraServlet
+import org.scalatra.{Ok, ScalatraServlet}
 import org.scalatra.json.JacksonJsonSupport
-import org.scalatra.swagger.SwaggerSupport
+import org.scalatra.swagger.{Swagger, SwaggerSupport}
 
 trait KoutaServlet extends ScalatraServlet with SwaggerSupport with JacksonJsonSupport {
   override protected implicit def jsonFormats: Formats = DefaultFormats +
     new EnumNameSerializer(Julkaisutila) + new EnumNameSerializer(Koulutustyyppi)
+
+  before() {
+    contentType = formats("json")
+  }
+}
+
+class HealthcheckServlet(implicit val swagger:Swagger) extends KoutaServlet {
+
+  override val applicationDescription = "Healthcheck API"
+
+  import org.json4s.JsonDSL._
+
+  get("/", operation(apiOperation[String]("Healthcheck") summary "Healthcheck")) {
+    Ok("message" -> "ok")
+  }
 }
 
