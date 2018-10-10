c253554583fcba9fa563d1c67b460549d5d667b4
Jessika Penttinen <jessika.penttinen@siili.com>
2018-10-10 15:29:33 +0300

    Mountataan ToteutusServlet

--- src/main/scala/fi/oph/kouta/ScalatraBootstrap.scala
+++ src/main/scala/fi/oph/kouta/ScalatraBootstrap.scala
@@ -1,7 +1,7 @@
 import fi.oph.kouta.config.{KoutaConfiguration, KoutaConfigurationFactory}
 import fi.oph.kouta.repository.KoutaDatabase
 import fi.oph.kouta.{KoutaBackendSwagger, SwaggerServlet}
-import fi.oph.kouta.servlet.{HealthcheckServlet, KoulutusServlet}
+import fi.oph.kouta.servlet.{HealthcheckServlet, KoulutusServlet, ToteutusServlet}
 import fi.vm.sade.utils.slf4j.Logging
 import org.scalatra._
 import javax.servlet.ServletContext
@@ -18,6 +18,7 @@ class ScalatraBootstrap extends LifeCycle with Logging {
 
     context.mount(new HealthcheckServlet(), "/healthcheck", "healthcheck")
     context.mount(new KoulutusServlet(), "/koulutus", "koulutus")
+    context.mount(new ToteutusServlet(), "/toteutus", "toteutus")
 
     context.mount(new SwaggerServlet, "/swagger")
   }
