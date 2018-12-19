b9d9e074f85ba6f85860e29f075096528d18e9e8
Jessika Penttinen <jessika.penttinen@siili.com>
2018-12-19 15:32:37 +0200

    Korjattu luokan järjestystä

--- src/main/scala/fi/oph/kouta/client/OrganisaatioClient.scala
+++ src/main/scala/fi/oph/kouta/client/OrganisaatioClient.scala
@@ -9,25 +9,23 @@ import org.json4s.jackson.JsonMethods._
 object OrganisaatioClient extends HttpClient with KoutaJsonFormats {
   val urlProperties = KoutaConfigurationFactory.configuration.urlProperties
 
+  def getAllParentAndChildOidsFlat(oid: OrganisaatioOid): Seq[OrganisaatioOid] = getHierarkia(oid, parentsAndChildren(oid, _))
+
+  def getAllChildOidsFlat(oid: OrganisaatioOid): Seq[OrganisaatioOid] = getHierarkia(oid, children(oid, _))
+
+  case class OrganisaatioResponse(numHits: Int, organisaatiot: List[OidAndChildren])
+  case class OidAndChildren(oid: OrganisaatioOid, children: List[OidAndChildren], parentOidPath: String)
+
+  private def getHierarkia[R](oid: OrganisaatioOid, result: (List[OidAndChildren]) => R) = get(
+    urlProperties.url("organisaatio-service.organisaatio.hierarkia", queryParams(oid.toString)),
+    (r) => result(parse(r).extract[OrganisaatioResponse].organisaatiot))
+
   private def queryParams(oid: String) =
     toQueryParams(
       "oid" -> oid,
       "aktiiviset" -> "true",
       "suunnitellut" -> "true",
       "lakkautetut" -> "false")
-
-  private def getHierarkia[R](oid: OrganisaatioOid, result: (List[OidAndChildren]) => R) = get(
-    urlProperties.url("organisaatio-service.organisaatio.hierarkia", queryParams(oid.toString)),
-    (r) => result(parse(r).extract[OrganisaatioResponse].organisaatiot))
-
-
-  def getAllParentAndChildOidsFlat(oid: OrganisaatioOid): Seq[OrganisaatioOid] = getHierarkia(oid, parentsAndChildren(oid, _))
-
-  def getAllChildOidsFlat(oid: OrganisaatioOid): Seq[OrganisaatioOid] = getHierarkia(oid, children(oid, _))
-
-  case class OrganisaatioResponse(numHits: Int, organisaatiot: List[OidAndChildren])
-
-  case class OidAndChildren(oid: OrganisaatioOid, children: List[OidAndChildren], parentOidPath: String)
 
   private def children(oid: OrganisaatioOid, organisaatiot: List[OidAndChildren]): Seq[OrganisaatioOid] =
     find(oid, organisaatiot).map(x => x.oid +: childOisFlat(x)).getOrElse(Seq()).distinct
