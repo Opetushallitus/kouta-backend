5436eb661f492ddf5b0ff848a679766dc53e8732
Jessika Penttinen <jessika.penttinen@siili.com>
2019-03-29 10:10:09 +0200

    Vaihdettu testisalasana lyhyempään ja välilyönnittömään

--- src/test/scala/fi/oph/kouta/embeddedJettyLauncher.scala
+++ src/test/scala/fi/oph/kouta/embeddedJettyLauncher.scala
@@ -44,8 +44,8 @@ object TestSetups extends Logging with KoutaConfigurationConstants {
 
   def setupAwsKeysForSqs() = {
     if(!Option(System.getProperty("aws.accessKeyId", null)).isDefined) {
-      System.setProperty("aws.accessKeyId", "just need something for Localstack")
-      System.setProperty("aws.secretKey", "just need something for Localstack")
+      System.setProperty("aws.accessKeyId", "randomKeyIdForLocalstack")
+      System.setProperty("aws.secretKey", "randomKeyForLocalstack")
     }
   }
 
