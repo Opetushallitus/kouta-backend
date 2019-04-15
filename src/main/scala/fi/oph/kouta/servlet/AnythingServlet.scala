aeb398f53b2ec7f0d605d71e389fe2a78e327eea
Topias Heinonen <topias.heinonen@solinor.com>
2019-04-15 13:40:06 +0300

    KTO-134 Lisätty autentikoinnin tarkistus anything-endpointteihin

--- src/main/scala/fi/oph/kouta/servlet/AnythingServlet.scala
+++ src/main/scala/fi/oph/kouta/servlet/AnythingServlet.scala
@@ -7,7 +7,7 @@ import fi.oph.kouta.service.ModificationService
 import org.scalatra.Ok
 import org.scalatra.swagger.Swagger
 
-class AnythingServlet(implicit val swagger:Swagger) extends KoutaServlet {
+class AnythingServlet(implicit val swagger:Swagger) extends KoutaServlet with CasAuthenticatedServlet {
   override val modelName: String = "Anything"
   override val applicationDescription = "Tarjonnan muutosten API"
 
@@ -15,6 +15,8 @@ class AnythingServlet(implicit val swagger:Swagger) extends KoutaServlet {
     tags modelName
     summary "Hae kaikki tietyn ajan jälkeen muuttuneet oidit"
     parameter pathParam[String]("since").description(SampleHttpDate))) {
+
+    implicit val authenticated: Authenticated = authenticate
 
     Ok(ModificationService.getModifiedSince(parseHttpDate(URLDecoder.decode(params("since"), "UTF-8"))))
   }
