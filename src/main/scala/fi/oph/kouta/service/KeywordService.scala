d67815a1c8db34a342c79f54adce3e56428c05e2
Topias Heinonen <topias.heinonen@gofore.com>
2019-06-11 13:36:40 +0300

    KTO-128 Lisätty pääsynhallinta asiasana-endpointeille

--- src/main/scala/fi/oph/kouta/service/KeywordService.scala
+++ src/main/scala/fi/oph/kouta/service/KeywordService.scala
@@ -2,10 +2,15 @@ package fi.oph.kouta.service
 
 import fi.oph.kouta.domain.keyword.{Keyword, KeywordSearch, KeywordType}
 import fi.oph.kouta.repository.KeywordDAO
+import fi.oph.kouta.security.{Role, RoleEntity}
+import fi.oph.kouta.servlet.Authenticated
 
-object KeywordService {
+object KeywordService extends AuthorizationService {
+
+  val roleEntity: RoleEntity = Role.Haku
 
   def search(search: KeywordSearch): List[String] = KeywordDAO.search(search)
 
-  def store(`type`: KeywordType, keywords: List[Keyword]): Int = KeywordDAO.put(`type`, keywords)
+  def store(`type`: KeywordType, keywords: List[Keyword])(implicit authenticated: Authenticated): Int =
+    withRootAccess(RoleEntity.all.flatMap(_.createRoles)) (KeywordDAO.put(`type`, keywords))
 }
