70f42fca4bc1b739e3da5ec766565497415dbca6
Topias Heinonen <topias.heinonen@gofore.com>
2019-12-10 15:39:01 +0200

    Lisätty access log

--- src/main/scala/fi/oph/kouta/JettyLauncher.scala
+++ src/main/scala/fi/oph/kouta/JettyLauncher.scala
@@ -1,9 +1,12 @@
 package fi.oph.kouta
 
-import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.webapp.WebAppContext
+import ch.qos.logback.access.jetty.RequestLogImpl
+import fi.oph.kouta.config.KoutaConfigurationFactory
+import fi.vm.sade.properties.OphProperties
 import fi.vm.sade.utils.slf4j.Logging
+import org.eclipse.jetty.server.{RequestLog, Server}
 import org.eclipse.jetty.util.resource.Resource
+import org.eclipse.jetty.webapp.WebAppContext
 
 object JettyLauncher extends Logging {
   val DEFAULT_PORT = "8080"
@@ -23,8 +26,23 @@ class JettyLauncher(val port: Int) {
   context.setContextPath("/kouta-backend")
   server.setHandler(context)
 
+  server.setRequestLog(requestLog(KoutaConfigurationFactory.configuration.urlProperties))
+
   def start = {
     server.start
     server
+  }
+
+  private def requestLog(properties: OphProperties): RequestLog = {
+    val requestLog = new RequestLogImpl
+    val logbackAccess = properties.getOrElse("logback.access", null)
+    if (logbackAccess != null) {
+      requestLog.setFileName(logbackAccess)
+    } else {
+      println("JettyLauncher: Jetty access log is printed to console, use -Dlogback.access to set configuration file")
+      requestLog.setResource("/logback-access.xml")
+    }
+    requestLog.start()
+    requestLog
   }
 }
