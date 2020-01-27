f044feab79cc0102720488efac465c8ebfd854a1
Jani Rahkola <jani.rahkola@reaktor.com>
2020-01-27 16:53:33 +0200

    Puuttuva kohdejoukonTarkenneKoodiUri
    Mahdollista kohdejoukonTarkenneKoodiUri arvon poistaminen haku fixtuurista
    asettamalla avaimen KohdejoukonTarkenneKoodiUriKey arvoksi null.

--- src/test/scala/fi/oph/kouta/external/KoutaFixtureTool.scala
+++ src/test/scala/fi/oph/kouta/external/KoutaFixtureTool.scala
@@ -360,7 +360,7 @@ object KoutaFixtureTool extends KoutaJsonFormats {
       Some(params(AlkamiskausiKoodiUriKey)),
       Some(params(AlkamisvuosiKey)),
       Some(params(KohdejoukkoKoodiUriKey)),
-      Some(params(KohdejoukonTarkenneKoodiUriKey)),
+      params.get(KohdejoukonTarkenneKoodiUriKey).flatMap(Option(_)),
       Some(Hakulomaketyyppi.withName(params(HakulomaketyyppiKey))),
       params.get(HakulomakeIdKey).map(UUID.fromString),
       toKielistetty(kielivalinta, params(HakulomakeKuvausKey)),
