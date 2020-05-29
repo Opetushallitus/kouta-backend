46345e7c71d5460e55ce3f4578a9ce503ba3f6fb
Jessika Penttinen <jessika.penttinen@siili.com>
2020-05-29 10:52:59 +0300

    Lisätty kommentit uusiin tietokantakenttiin

---
+++ src/main/resources/db/migration/V48__add_valintakoe_comments.sql
@@ -0,0 +1,7 @@
+comment on column hakukohteet.metadata IS 'Yleistä hakukohteeseen liittyvää metadataa, mm. valintakokeiden yleiskuvaus';
+
+comment on column hakukohteiden_valintakokeet.metadata is 'Valintakokeen Opintopolussa näytettävä nimi eri kielillä. Kielet on määritetty hakukohteen kielivalinnassa.';
+comment on column hakukohteiden_valintakokeet.metadata is 'Yleistä valintakokeeseen liittyvää metadataa, mm. tietoa erityisjärjestelyistä ja ennakkovalmistautumisesta';
+
+comment on column valintaperusteiden_valintakokeet.metadata is 'Valintakokeen Opintopolussa näytettävä nimi eri kielillä. Kielet on määritetty valintaperustekuvauksen kielivalinnassa.';
+comment on column valintaperusteiden_valintakokeet.metadata is 'Yleistä valintakokeeseen liittyvää metadataa, mm. tietoa erityisjärjestelyistä ja ennakkovalmistautumisesta';
