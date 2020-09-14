06eca496165c5b07d87a5d46f279c7cba7af8980
Jessika Penttinen <jessika.penttinen@siili.com>
2020-09-14 09:07:04 +0300

    Päivitetty cas clientin versio, jotta saadaan caller id kutsuihin

--- kouta-backend/src/main/scala/fi/oph/kouta/security/securityContext.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/security/securityContext.scala
@@ -2,6 +2,7 @@ package fi.oph.kouta.security
 
 import fi.oph.kouta.config.SecurityConfiguration
 import fi.vm.sade.utils.cas.CasClient
+import fi.oph.kouta.client.CallerId
 
 trait SecurityContext {
   def casUrl: String
@@ -13,9 +14,9 @@ case class ProductionSecurityContext(casUrl: String,
                                      casClient: CasClient,
                                      casServiceIdentifier: String) extends SecurityContext
 
-object ProductionSecurityContext {
+object ProductionSecurityContext extends CallerId {
   def apply(config: SecurityConfiguration): ProductionSecurityContext = {
-    val casClient = new CasClient(config.casUrl, org.http4s.client.blaze.defaultClient)
+    val casClient = new CasClient(config.casUrl, org.http4s.client.blaze.defaultClient, callerId)
     ProductionSecurityContext(config.casUrl, casClient, config.casServiceIdentifier)
   }
 }
