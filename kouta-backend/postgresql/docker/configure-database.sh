bd6be9e76ed43fede901fdbba0dc7e7eba811354
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2020-10-23 22:51:15 +0300

    postgres kontin luonti

---
+++ kouta-backend/postgresql/docker/configure-database.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DB_APP_DB=kouta
+DB_APP_USER=oph
+DB_APP_PASSWORD=oph
+
+echo "Creating database \"$DB_APP_DB\", creating role \"$DB_APP_USER\" with database owner privileges…"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-END
+create role "${DB_APP_USER}" with password '${DB_APP_PASSWORD}' login;
+create database "${DB_APP_DB}" encoding 'UTF-8';
+grant all privileges on database "${DB_APP_DB}" to "${DB_APP_USER}";
+ALTER SCHEMA public OWNER TO "${DB_APP_USER}";
+GRANT ALL ON SCHEMA public TO "${DB_APP_USER}";
+END
