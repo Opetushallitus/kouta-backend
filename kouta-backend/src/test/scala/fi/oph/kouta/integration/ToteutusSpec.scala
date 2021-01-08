589aaba1627b2c9e78e271b7737335a37114499f
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-01-08 12:41:23 +0200

    KTO-1036: Testi uudelle rakenteelle

--- kouta-backend/src/test/scala/fi/oph/kouta/integration/ToteutusSpec.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/integration/ToteutusSpec.scala
@@ -482,10 +482,24 @@ class ToteutusSpec extends KoutaIntegrationSpec
     update(thisToteutusWithOid.copy(tila = Arkistoitu), lastModified)
   }
 
-
+  //feature flag KTO-1036 remove this test
   it should "not validate dates when updating a julkaistu toteutus" in {
     val (past, morePast) = (TestData.inPast(5000), TestData.inPast(60000))
     val inPastOpetus = opetus.copy(koulutuksenAlkamispaivamaara = Some(morePast), koulutuksenPaattymispaivamaara = Some(past))
+    val inPastMetadata = ammMetatieto.copy(opetus = Some(inPastOpetus))
+
+    val oid = put(toteutus(koulutusOid).copy(tila = Julkaistu))
+    val thisToteutus = toteutus(oid, koulutusOid)
+
+    val lastModified = get(oid, thisToteutus)
+
+    update(thisToteutus.copy(metadata = Some(inPastMetadata)), lastModified)
+  }
+
+  //feature flag KTO-1036
+  it should "not validate dates when updating a julkaistu toteutus UUSI" in {
+    val (past, morePast) = (TestData.inPast(5000), TestData.inPast(60000))
+    val inPastOpetus = opetus.copy(koulutuksenAlkamiskausiUUSI = pastAlkamiskausi(alkamispvm = morePast, paattymispvm = past))
     val inPastMetadata = ammMetatieto.copy(opetus = Some(inPastOpetus))
 
     val oid = put(toteutus(koulutusOid).copy(tila = Julkaistu))
