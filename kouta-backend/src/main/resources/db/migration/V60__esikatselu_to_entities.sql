02ef3c0b3d95f16bd3bcd581e566399ca7f383ea
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-02-05 08:31:19 +0200

    KTO-443: Lisätty koulutukselle esikatselu-kenttä kantaan

--- kouta-backend/src/main/resources/db/migration/V60__esikatselu_to_entities.sql
+++ kouta-backend/src/main/resources/db/migration/V60__esikatselu_to_entities.sql
@@ -79,3 +79,51 @@ begin
     return null;
 end;
 $$;
+
+alter table koulutukset
+    add column esikatselu boolean not null default false;
+alter table koulutukset_history
+    add column esikatselu boolean not null default false;
+
+comment on column koulutukset.esikatselu is 'Onko koulutus nähtävissä esikatselussa';
+
+create or replace function update_koulutukset_history() returns trigger
+    language plpgsql
+as
+$$
+begin
+    insert into koulutukset_history (oid,
+                                     johtaa_tutkintoon,
+                                     tyyppi,
+                                     koulutus_koodi_uri,
+                                     tila,
+                                     nimi,
+                                     metadata,
+                                     muokkaaja,
+                                     transaction_id,
+                                     system_time,
+                                     kielivalinta,
+                                     organisaatio_oid,
+                                     esikatselu,
+                                     julkinen,
+                                     teemakuva,
+                                     eperuste_id)
+    values (old.oid,
+            old.johtaa_tutkintoon,
+            old.tyyppi,
+            old.koulutus_koodi_uri,
+            old.tila,
+            old.nimi,
+            old.metadata,
+            old.muokkaaja,
+            old.transaction_id,
+            tstzrange(lower(old.system_time), now(), '[)'),
+            old.kielivalinta,
+            old.organisaatio_oid,
+            old.esikatselu,
+            old.julkinen,
+            old.teemakuva,
+            old.eperuste_id);
+    return null;
+end;
+$$;
