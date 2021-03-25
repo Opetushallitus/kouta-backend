9692a117d625681b71aa3af94b6961120507a1e6
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-03-25 11:38:59 +0200

    KTO-283: Poistettu ei korkeakoulutus koulutuksilta testidatasta useampi koulutusKoodiUri

--- kouta-backend/src/test/scala/fi/oph/kouta/TestData.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/TestData.scala
@@ -71,7 +71,7 @@ object TestData {
     esikatselu = true,
     koulutustyyppi = Amm,
     koulutusKoodiUri = Some("koulutus_371101#1"),
-    koulutuksetKoodiUri = Seq("koulutus_371101#1", "koulutus_201000#1"),
+    koulutuksetKoodiUri = Seq("koulutus_371101#1"),
     tila = Julkaistu,
     nimi = Map(Fi -> "nimi", Sv -> "nimi sv"),
     metadata = Some(AmmatillinenKoulutusMetadata(
@@ -137,7 +137,7 @@ object TestData {
   val AmmOsaamisalaKoulutus: Koulutus = AmmTutkinnonOsaKoulutus.copy(
     koulutustyyppi = AmmOsaamisala,
     koulutusKoodiUri = Some("koulutus_371101#1"),
-    koulutuksetKoodiUri = Seq("koulutus_371101#1", "koulutus_201000#1"),
+    koulutuksetKoodiUri = Seq("koulutus_371101#1"),
     ePerusteId = Some(11L),
     metadata = Some(AmmatillinenOsaamisalaKoulutusMetadata(
       tyyppi = AmmOsaamisala,
