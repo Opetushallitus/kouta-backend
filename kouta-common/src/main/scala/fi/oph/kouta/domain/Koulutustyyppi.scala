15127b0aed39cd47f1cb1d5e3928861d1727437e
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2021-09-16 13:34:47 +0300

    KTO-187: Lisätään telma-koulutustyyppi kouta-backendiin

--- kouta-common/src/main/scala/fi/oph/kouta/domain/Koulutustyyppi.scala
+++ kouta-common/src/main/scala/fi/oph/kouta/domain/Koulutustyyppi.scala
@@ -5,7 +5,7 @@ sealed trait Koulutustyyppi extends EnumType
 object Koulutustyyppi extends Enum[Koulutustyyppi] {
   override def name: String = "koulutustyyppi"
 
-  def values = List(Amm, Lk, Muu, Yo, Amk, Tuva, AmmTutkinnonOsa, AmmOsaamisala)
+  def values = List(Amm, Lk, Muu, Yo, Amk, Tuva, Telma, AmmTutkinnonOsa, AmmOsaamisala)
 
   def ammatilliset = List(Amm, AmmTutkinnonOsa, AmmOsaamisala)
   def korkeakoulu = List(Amk, Yo)
@@ -71,6 +71,7 @@ case object Lk extends Koulutustyyppi { val name = "lk" }
 case object Yo extends Koulutustyyppi { val name = "yo" }
 case object Amk extends Koulutustyyppi { val name = "amk" }
 case object Tuva extends Koulutustyyppi { val name = "tuva" }
+case object Telma extends Koulutustyyppi { val name = "telma" }
 case object Muu extends Koulutustyyppi {val name = "muu"}
 case object AmmTutkinnonOsa extends Koulutustyyppi {val name = "amm-tutkinnon-osa"}
 case object AmmOsaamisala extends Koulutustyyppi {val name = "amm-osaamisala"}
