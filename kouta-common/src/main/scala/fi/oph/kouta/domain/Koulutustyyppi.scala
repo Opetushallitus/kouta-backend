ec7509cfdc90cbb68632b0450ae3402ae4bf3865
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2021-09-27 16:31:32 +0300

    KTO-1359: Korjataan koulutustyyppi to oppilaitostyyppi -mäppäyksiä

--- kouta-common/src/main/scala/fi/oph/kouta/domain/Koulutustyyppi.scala
+++ kouta-common/src/main/scala/fi/oph/kouta/domain/Koulutustyyppi.scala
@@ -39,12 +39,12 @@ object Koulutustyyppi extends Enum[Koulutustyyppi] {
     "oppilaitostyyppi_01#1" -> Seq(Muu), //Taiteen perusopetuksen oppilaitokset (ei musiikki)
     "oppilaitostyyppi_11#1" -> Seq(Muu, Tuva), //Peruskoulut
     "oppilaitostyyppi_12#1" -> Seq(Muu, Tuva), //Peruskouluasteen erityiskoulut
-    "oppilaitostyyppi_15#1" -> Seq(Lk, Tuva), //Lukiot
-    "oppilaitostyyppi_19#1" -> Seq(Lk, Tuva), //Perus- ja lukioasteen koulut
-    "oppilaitostyyppi_21#1" -> Seq(Amm, Tuva, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Ammatilliset oppilaitokset
+    "oppilaitostyyppi_15#1" -> Seq(Lk, Muu, Tuva), //Lukiot
+    "oppilaitostyyppi_19#1" -> Seq(Lk, Muu, Tuva), //Perus- ja lukioasteen koulut
+    "oppilaitostyyppi_21#1" -> Seq(Amm, Lk, Muu, Tuva, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Ammatilliset oppilaitokset
     "oppilaitostyyppi_22#1" -> Seq(Amm, Tuva), //Ammatilliset erityisoppilaitokset
-    "oppilaitostyyppi_23#1" -> Seq(Amm, Tuva), //Ammatilliset erikoisoppilaitokset
-    "oppilaitostyyppi_24#1" -> Seq(Amm, Tuva), //Ammatilliset aikuiskoulutuskeskukset
+    "oppilaitostyyppi_23#1" -> Seq(Amm), //Ammatilliset erikoisoppilaitokset
+    "oppilaitostyyppi_24#1" -> Seq(Amm), //Ammatilliset aikuiskoulutuskeskukset
     "oppilaitostyyppi_28#1" -> Seq(Amm), //Palo-, poliisi- ja vartiointialojen oppilaitokset
     "oppilaitostyyppi_29#1" -> Seq(Amm), //Sotilasalan ammatilliset oppilaitokset
     "oppilaitostyyppi_41#1" -> Seq(Amk), //Ammattikorkeakoulut
@@ -52,16 +52,16 @@ object Koulutustyyppi extends Enum[Koulutustyyppi] {
     "oppilaitostyyppi_43#1" -> Seq(Yo), //Sotilaskorkeakoulut
     "oppilaitostyyppi_45#1" -> Seq(Yo), //Lastentarhanopettajaopistot
     "oppilaitostyyppi_46#1" -> Seq(Amk), //Väliaikaiset ammattikorkeakoulut
-    "oppilaitostyyppi_61#1" -> Seq(Muu), //Musiikkioppilaitokset
-    "oppilaitostyyppi_62#1" -> Seq(Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Liikunnan koulutuskeskukset
-    "oppilaitostyyppi_63#1" -> Seq(Amm, Muu, Tuva, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Kansanopistot
-    "oppilaitostyyppi_64#1" -> Seq(Muu, Tuva, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Kansalaisopistot
-    "oppilaitostyyppi_65#1" -> Seq(Muu), //Opintokeskukset
-    "oppilaitostyyppi_66#1" -> Seq(Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Kesäyliopistot
+    "oppilaitostyyppi_61#1" -> Seq(Amm, Muu), //Musiikkioppilaitokset
+    "oppilaitostyyppi_62#1" -> Seq(Amm, Muu, Tuva, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Liikunnan koulutuskeskukset
+    "oppilaitostyyppi_63#1" -> Seq(Amm, Lk, Muu, Tuva, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Kansanopistot
+    "oppilaitostyyppi_64#1" -> Seq(Amm, Lk, Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Kansalaisopistot
+    "oppilaitostyyppi_65#1" -> Seq(Amm, Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Opintokeskukset
+    "oppilaitostyyppi_66#1" -> Seq(Amm, Lk, Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Kesäyliopistot
     "oppilaitostyyppi_91#1" -> Seq(Muu), //Kirjeoppilaitokset
     "oppilaitostyyppi_92#1" -> Seq(Muu), //Neuvontajärjestöt
-    "oppilaitostyyppi_93#1" -> Seq(Muu), //Muut koulutuksen järjestäjät
-    "oppilaitostyyppi_99#1" -> Seq(Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Muut oppilaitokset
+    "oppilaitostyyppi_93#1" -> Seq(Amm, Muu), //Muut koulutuksen järjestäjät
+    "oppilaitostyyppi_99#1" -> Seq(Amm, Muu, VapaaSivistystyoOpistovuosi, VapaaSivistystyoMuu), //Muut oppilaitokset
     "oppilaitostyyppi_XX#1" -> Seq(Muu), //Ei tiedossa (oppilaitostyyppi)
   )
 }
