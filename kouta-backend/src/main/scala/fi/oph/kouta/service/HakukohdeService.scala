0a112187c04d40c5a8f1d4b8cf0d734ab9421db7
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2021-10-26 09:53:36 +0300

    KTO-1382: Sallitaan hakukohteen julkaisu vaikka haku ei ole julkaistu

--- kouta-backend/src/main/scala/fi/oph/kouta/service/HakukohdeService.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/service/HakukohdeService.scala
@@ -73,7 +73,7 @@ class HakukohdeService(sqsInTransactionService: SqsInTransactionService, auditLo
 
     throwValidationErrors(and(
       validateDependency(hakukohde.tila, deps.get(toteutusOid).map(_._1), toteutusOid, "Toteutusta", "toteutusOid"),
-      validateDependency(hakukohde.tila, deps.get(hakuOid).map(_._1), hakuOid, "Hakua", "hakuOid"),
+      Validations.assertDependencyExists(deps.contains(hakuOid), hakuOid, "Hakua", "hakuOid"),
       validateIfDefined[UUID](hakukohde.valintaperusteId, valintaperusteId => and(
         validateDependency(hakukohde.tila, deps.get(valintaperusteId.toString).map(_._1), valintaperusteId, "Valintaperustetta", "valintaperusteId"),
         validateIfDefined[Koulutustyyppi](deps.get(valintaperusteId.toString).flatMap(_._2), valintaperusteTyyppi =>
