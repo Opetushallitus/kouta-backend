ea5509c39f12696af1f8e16e0f2e6ffc0704a5d8
Petteri Tolonen <petteri.tolonen@knowit.fi>
2021-11-26 13:46:28 +0200

    KTO-1371: 5 minuutin cache koodisto- ja lokalisointi-clientteihin

--- kouta-backend/src/main/scala/fi/oph/kouta/client/KoodistoClient.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/client/KoodistoClient.scala
@@ -6,32 +6,42 @@ import fi.oph.kouta.config.KoutaConfigurationFactory
 import fi.vm.sade.properties.OphProperties
 import fi.oph.kouta.domain.{Fi, Sv, En, Kielistetty}
 import org.json4s.{DefaultFormats}
+import scalacache.caffeine._
+import scalacache.memoization.memoizeSync
+import scalacache.modes.sync._
+import scala.concurrent.duration._
 
 object KoodistoClient extends KoodistoClient(KoutaConfigurationFactory.configuration.urlProperties)
 
 class KoodistoClient(urlProperties: OphProperties) extends HttpClient with CallerId with Logging {
-  implicit val formats = DefaultFormats
-  case class CodeElementMetadataItem(nimi: String, kieli: String) {}
-  case class CodeElement(koodiUri: String, metadata: List[CodeElementMetadataItem] = List()) {}
+  implicit val formats       = DefaultFormats
+  implicit val KoodistoCache = CaffeineCache[Map[String, Kielistetty]]
+
+  case class CodeElementMetadataItem(nimi: String, kieli: String)
+  case class CodeElement(koodiUri: String, metadata: List[CodeElementMetadataItem] = List())
 
   def parseKoodi(codeElement: CodeElement): Kielistetty = {
     codeElement.metadata
       .withFilter(metaItem => List("FI", "SV", "EN").contains(metaItem.kieli))
-      .map((metaItem: CodeElementMetadataItem) => (metaItem.kieli match {
-        case "FI" => Fi
-        case "SV" => Sv
-        case "EN" => En
-      }, metaItem.nimi))
+      .map((metaItem: CodeElementMetadataItem) =>
+        (
+          metaItem.kieli match {
+            case "FI" => Fi
+            case "SV" => Sv
+            case "EN" => En
+          },
+          metaItem.nimi
+        )
+      )
       .toMap
   }
   def parseKoodit(codeElements: List[CodeElement]): Map[String, Kielistetty] = {
     codeElements.map(codeElement => (codeElement.koodiUri, parseKoodi(codeElement))).toMap
   }
-  def getKoodistoKaannokset(koodisto: String): Map[String, Kielistetty] = {
-    get(urlProperties.url("koodisto-service.koodisto-koodit", koodisto), followRedirects = true) {
-      response => {
+  def getKoodistoKaannokset(koodisto: String): Map[String, Kielistetty] =
+    memoizeSync[Map[String, Kielistetty]](Some(5.minutes)) {
+      get(urlProperties.url("koodisto-service.koodisto-koodit", koodisto), followRedirects = true) { response =>
         parseKoodit(parse(response).extract[List[CodeElement]])
       }
     }
-  }
 }
