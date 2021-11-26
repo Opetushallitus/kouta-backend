c0e171a3ec627191e1e7441c47117079848aebd0
Petteri Tolonen <petteri.tolonen@knowit.fi>
2021-11-26 13:46:28 +0200

    KTO-1371: Lisätty funktio lukio-toteutuksen nimen muodostamiseen

--- kouta-backend/src/main/scala/fi/oph/kouta/util/NameHelper.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/util/NameHelper.scala
@@ -1,8 +1,85 @@
 package fi.oph.kouta.util
 
-import fi.oph.kouta.domain.{Fi, Kielistetty, ToteutusMetadata}
+import fi.oph.kouta.domain.{
+  En,
+  Fi,
+  Kieli,
+  Kielistetty,
+  Koulutus,
+  LukioKoulutusMetadata,
+  LukioToteutusMetadata,
+  LukiolinjaTieto,
+  Sv,
+  Toteutus,
+  ToteutusMetadata
+}
 
 object NameHelper {
+  val EmptyKielistetty: Kielistetty = Map(
+    Fi -> "",
+    Sv -> "",
+    En -> ""
+  )
+  def localizedKielistetty(kielistetty: Option[Kielistetty], kieli: Kieli): String = {
+    kielistetty.getOrElse(EmptyKielistetty).getOrElse(kieli, "")
+  }
+  def isEmptyKielistetty(kielistetty: Option[Kielistetty]): Boolean =
+    kielistetty.isDefined && kielistetty.get.forall { case (lng, translation) => translation.isEmpty }
+  def generateLaajuus(laajuus: Option[Kielistetty], opintopistetta: Option[Kielistetty], lng: Kieli): String = {
+    if (!isEmptyKielistetty(laajuus) && !isEmptyKielistetty(opintopistetta)) {
+      localizedKielistetty(laajuus, lng) + " " + localizedKielistetty(opintopistetta, lng)
+    } else {
+      ""
+    }
+  }
+  def getLukiolinjat(toteutus: Toteutus): Option[Seq[LukiolinjaTieto]] = {
+    toteutus.metadata match {
+      case Some(toteutusMetadata) =>
+        toteutusMetadata match {
+          case lukio: LukioToteutusMetadata => Some(lukio.painotukset ++ lukio.erityisetKoulutustehtavat)
+        }
+    }
+  }
+  def generateLukioToteutusDisplayName(
+      toteutus: Toteutus,
+      koulutus: Koulutus,
+      kaannokset: Map[String, Kielistetty],
+      koodiKaannokset: Map[String, Kielistetty]
+  ): Kielistetty = {
+    val yleislinjaNimiOsa = kaannokset.get("toteutuslomake.lukionYleislinjaNimiOsa")
+    val opintopistetta    = kaannokset.get("yleiset.opintopistetta")
+    val lukiolinjat       = getLukiolinjat(toteutus)
+    val hasYleislinja = (for {
+      m <- toteutus.metadata.asInstanceOf[Option[LukioToteutusMetadata]]
+    } yield m.yleislinja).getOrElse(false)
+
+    val lukiolinjaKaannokset = (if (hasYleislinja) Seq(yleislinjaNimiOsa) else Seq()) ++
+      lukiolinjat.get.map(l => koodiKaannokset.get(l.koodiUri)).filter(!isEmptyKielistetty(_))
+
+    val laajuusKaannos = for {
+      m <- koulutus.metadata
+      l <- m.asInstanceOf[LukioKoulutusMetadata].opintojenLaajuusKoodiUri
+      k <- koodiKaannokset.get(l)
+    } yield k
+
+    List(Fi, Sv, En)
+      .map(lng =>
+        (
+          lng,
+          lukiolinjaKaannokset
+            .map(linjaKaannos => {
+              val linjaTranslation = localizedKielistetty(linjaKaannos, lng)
+              if (linjaTranslation.nonEmpty)
+                linjaTranslation + ", " + generateLaajuus(laajuusKaannos, opintopistetta, lng)
+              else ""
+            })
+            .mkString("\n")
+            .trim
+        )
+      )
+      .filter { case (_, kaannos) => kaannos.nonEmpty }
+      .toMap
+  }
   def generateHakukohdeDisplayNameForTuva(
     hakukohdeNimi: Kielistetty,
     toteutusMetadata: ToteutusMetadata,
--- kouta-backend/src/test/scala/fi/oph/kouta/util/NameHelperSpec.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/util/NameHelperSpec.scala
@@ -1,12 +1,15 @@
 package fi.oph.kouta.util
-import fi.oph.kouta.TestData.{JulkaistuHakukohde, TelmaToteutuksenMetatieto, TuvaToteutuksenMetatieto, TuvaToteutus}
-import fi.oph.kouta.domain._
+import com.softwaremill.diffx.scalatest.DiffMatcher.matchTo
+import fi.oph.kouta.TestData.{JulkaistuHakukohde, LukioKoulutus, LukioToteutuksenMetatieto, LukioToteutus, TelmaToteutuksenMetatieto, TuvaToteutuksenMetatieto, TuvaToteutus}
+import fi.oph.kouta.domain.{En, Fi, Hakukohde, Kielistetty, Koulutus, Sv, Toteutus, TelmaToteutusMetadata, TuvaToteutusMetadata}
+import com.softwaremill.diffx.scalatest.DiffMatcher._
+import com.softwaremill.diffx.generic.auto._
 
 class NameHelperSpec extends UnitSpec {
   val tuvaToteutus: Toteutus                        = TuvaToteutus
   val tuvaToteutuksenMetadata: TuvaToteutusMetadata = TuvaToteutuksenMetatieto
   val hakukohde: Hakukohde                          = JulkaistuHakukohde
-  val kaannokset: Kielistetty = Map(
+  val hakukohdeKaannokset: Kielistetty = Map(
     Fi -> "vaativana erityisenä tukena",
     Sv -> "krävande särskilt stöd",
     En -> "demanding special support"
@@ -22,7 +25,7 @@ class NameHelperSpec extends UnitSpec {
             Sv -> "Utbildning som handleder för arbete och ett självständigt liv (TELMA)"
           ),
         telmaToteutuksenMetadata,
-        kaannokset
+        hakukohdeKaannokset
       ) === Map(
         Fi -> "Työhön ja itsenäiseen elämään valmentava koulutus (TELMA)",
         Sv -> "Utbildning som handleder för arbete och ett självständigt liv (TELMA)"
@@ -39,8 +42,8 @@ class NameHelperSpec extends UnitSpec {
             Fi -> "Tutkintokoulutukseen valmentava koulutus (TUVA)",
             Sv -> "Utbildning som handleder för examensutbildning (Hux)"
           ),
-        tuvaToMetadataWithoutErityisopetus,
-        kaannokset
+            tuvaToMetadataWithoutErityisopetus,
+        hakukohdeKaannokset
       ) === Map(
         Fi -> "Tutkintokoulutukseen valmentava koulutus (TUVA)",
         Sv -> "Utbildning som handleder för examensutbildning (Hux)"
@@ -85,4 +88,45 @@ class NameHelperSpec extends UnitSpec {
       )
     )
   }
+  val toteutusKaannokset = Map(
+    "toteutuslomake.lukionYleislinjaNimiOsa" -> Map(Fi -> "Lukio", Sv -> "Gymnasium"),
+    "yleiset.opintopistetta" -> Map(Fi -> "opintopistettä", Sv -> "studiepoäng", En -> "credits")
+  )
+
+  val koodiKaannokset = Map(
+    "lukiopainotukset_1#1" -> Map(Fi -> "lukion painotus 1 fi", Sv -> "lukion painotus 1 sv"),
+    "lukiolinjaterityinenkoulutustehtava_1#1" -> Map(Fi -> "lukio erityinen koulutustehtävä 1 fi", Sv -> "lukio erityinen koulutustehtävä 1 sv"),
+    "opintojenlaajuus_40#1" -> Map(Fi -> "40", Sv -> "40"),
+  )
+
+  "generateToteutusDisplayName" should "generate Toteutus display name for lukio with yleislinja, painotus and erityinen koulutustehtävä" in {
+    val toteutus: Toteutus = LukioToteutus
+    val koulutus: Koulutus = LukioKoulutus
+    val esitysnimi = NameHelper.generateLukioToteutusDisplayName(
+        toteutus,
+        koulutus,
+        toteutusKaannokset,
+        koodiKaannokset
+    )
+    esitysnimi should matchTo(Map(
+        Fi -> "Lukio, 40 opintopistettä\nlukion painotus 1 fi, 40 opintopistettä\nlukio erityinen koulutustehtävä 1 fi, 40 opintopistettä",
+        Sv -> "Gymnasium, 40 studiepoäng\nlukion painotus 1 sv, 40 studiepoäng\nlukio erityinen koulutustehtävä 1 sv, 40 studiepoäng",
+      ))
+  }
+
+  it should "generate Toteutus display name for lukio with yleislinja only" in {
+    val koulutus: Koulutus = LukioKoulutus
+    val toteutus: Toteutus = LukioToteutus.copy(metadata = Some(LukioToteutuksenMetatieto.copy(painotukset = Seq(), erityisetKoulutustehtavat = Seq())))
+    val esitysnimi = NameHelper.generateLukioToteutusDisplayName(
+      toteutus,
+      koulutus,
+      toteutusKaannokset,
+      koodiKaannokset
+    )
+    esitysnimi should matchTo(Map(
+      Fi -> "Lukio, 40 opintopistettä",
+      Sv -> "Gymnasium, 40 studiepoäng",
+    ))
+  }
+
 }
