3fb893d70b59759249efb620f80f7a664673bb8c
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2022-02-04 09:46:46 +0200

    Revert "Napataan runtimeerror ja lähetetään tyhjä henkilötieto"
    This reverts commit 14183f5e0a73b440d20576a5acd34b34b66f8111.

--- kouta-backend/src/main/scala/fi/oph/kouta/client/OppijanumerorekisteriClient.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/client/OppijanumerorekisteriClient.scala
@@ -77,8 +77,6 @@ object OppijanumerorekisteriClient
       case error: CasClientException =>
         logger.error(s"Authentication to CAS failed: ${error}")
         Henkilo(kutsumanimi = None, sukunimi = None, etunimet = None)
-      case e: RuntimeException =>
-        Henkilo(kutsumanimi = None, sukunimi = None, etunimet = None)
     }
   }
 }
