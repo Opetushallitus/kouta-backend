143a08cc8508064dc4951f9456b1c7def103c050
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2022-03-22 11:32:09 +0200

    KTO-1446: Lisää koulutuksen alkamiskausi ja -vuosi search parametreihin

--- kouta-backend/src/main/scala/fi/oph/kouta/servlet/SearchServlet.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/servlet/SearchServlet.scala
@@ -13,7 +13,7 @@ class SearchServlet(koulutusService: KoulutusService,
 
   def this() = this(KoulutusService, ToteutusService, HakuService, HakukohdeService, ValintaperusteService)
 
-  val SearchParams = Seq("nimi", "koulutustyyppi", "muokkaaja", "tila", "julkinen", "hakutapa", "page", "size", "lng", "order-by", "order")
+  val SearchParams = Seq("nimi", "koulutustyyppi", "muokkaaja", "tila", "julkinen", "hakutapa", "koulutuksenAlkamiskausi", "koulutuksenAlkamisvuosi", "page", "size", "lng", "order-by", "order")
 
   val searchParams =
     """        - in: query
@@ -67,7 +67,23 @@ class SearchServlet(koulutusService: KoulutusService,
       |              type: string
       |          required: false
       |          description: Suodata pilkulla erotetuilla hakutapakoodiureilla
-      |          example: hakutapa_03
+      |          example: hakutapa_03#1
+      |        - in: query
+      |          name: koulutuksenAlkamiskausi
+      |          schema:
+      |            type: array
+      |            items:
+      |              type: string
+      |          required: false
+      |          description: Suodata koulutuksen alkamiskausikoodiureilla
+      |          example: kausi_s#1
+      |        - in: query
+      |          name: koulutuksenAlkamisvuosi
+      |          schema:
+      |            type: string
+      |          required: false
+      |          description: Suodata pilkulla erotetuilla vuosilla
+      |          example: 2022
       |        - in: query
       |          name: size
       |          schema:
