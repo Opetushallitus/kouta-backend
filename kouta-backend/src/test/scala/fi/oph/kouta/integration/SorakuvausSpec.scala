7a5050951e3c636f35ee4626230208fd3f69f3ec
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-06-10 15:04:17 +0300

    OY-3683: Testit sorakuvauksen tilasiirtymällä Julkaistu -> Tallennettu

--- kouta-backend/src/test/scala/fi/oph/kouta/integration/SorakuvausSpec.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/integration/SorakuvausSpec.scala
@@ -274,6 +274,19 @@ class SorakuvausSpec extends KoutaIntegrationSpec with AccessControlSpec with So
     }
   }
 
+  it should "allow oph user to update from julkaistu to tallennettu" in {
+    val oid = put(sorakuvaus)
+    val lastModified = get(oid, sorakuvaus(oid))
+    update(sorakuvaus(oid).copy(tila = Tallennettu), lastModified, expectUpdate = true, ophSession)
+    get(oid, sorakuvaus(oid).copy(tila = Tallennettu, muokkaaja = OphUserOid, metadata = Some(sorakuvaus.metadata.get.copy(isMuokkaajaOphVirkailija = Some(true)))))
+  }
+
+  it should "not allow non oph user to update from julkaistu to tallennettu" in {
+    val oid = put(sorakuvaus)
+    val lastModified = get(oid, sorakuvaus(oid))
+    update(sorakuvaus(oid).copy(tila = Tallennettu), lastModified, 403, crudSessions(ChildOid))
+  }
+
   it should "pass legal state changes" in {
     val id = put(sorakuvaus.copy(tila = Tallennettu), ophSession)
     var lastModified = get(id, sorakuvaus(id).copy(tila = Tallennettu))
