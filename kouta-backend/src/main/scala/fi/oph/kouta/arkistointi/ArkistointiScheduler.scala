110705d7bd7c89d5221138d01d4c25b716db500a
Tatu Honkanen <tatu.honkanen@cybercom.com>
2022-08-02 13:46:40 +0300

    OY-3337 : Logituksen parannusta

--- kouta-backend/src/main/scala/fi/oph/kouta/arkistointi/ArkistointiScheduler.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/arkistointi/ArkistointiScheduler.scala
@@ -21,12 +21,13 @@ class ArkistointiScheduler(hakuService: HakuService, auditLog: AuditLog) extends
 
   def this() = this(HakuService, AuditLog)
 
-  private val cronSchedule: Schedule     = new CronSchedule("1 0 * * * ?")
-  private val numberOfThreads: Int       = 1
-  private val user: User = new User(new Oid(RootOrganisaatioOid.toString), InetAddress.getLocalHost, "scheduler", "scheduler")
+  private val cronSchedule: Schedule = new CronSchedule("*/5 * * * * ?") //new CronSchedule("1 0 * * * ?")
+  private val numberOfThreads: Int   = 1
+  private val user: User =
+    new User(new Oid(RootOrganisaatioOid.toString), InetAddress.getLocalHost, "scheduler", "scheduler")
 
   private val executionHandler: VoidExecutionHandler[Void] = new VoidExecutionHandler[Void] {
-     override def execute(taskInstance: TaskInstance[Void], executionContext: ExecutionContext): Unit = {
+    override def execute(taskInstance: TaskInstance[Void], executionContext: ExecutionContext): Unit = {
       logger.info(s"Aloitetaan ajastettu hakujen ja hakukohteiden arkistointi.")
 
       try {
@@ -36,8 +37,8 @@ class ArkistointiScheduler(hakuService: HakuService, auditLog: AuditLog) extends
               logger.info(s"Lähetetään arkistoitu haku $hakuOid SQS-jonoon indeksoitavaksi.")
               hakuService.indexByOid(hakuOid)
             })
+          case _ =>
         }
-        //todo: varmista että Audit-logitus toimii
       } catch {
         case error: Exception => logger.error(s"Haun ja haukohteiden arkistointi epäonnistui: $error")
       }
@@ -63,35 +64,34 @@ class ArkistointiScheduler(hakuService: HakuService, auditLog: AuditLog) extends
 
   def runScheduler()(implicit authenticated: Authenticated): Unit = {
     logger.info(s"Käynnistetään käsin haun ja hakukohteiden arkistointi-scheduler.")
-  archiveHautJaHakukohteet(auditLog.getUser(authenticated))
+    archiveHautJaHakukohteet(auditLog.getUser(authenticated))
   }
 
   private def archiveHautJaHakukohteet(user: User): Option[Seq[HakuOid]] = {
-    logger.info("USER: " + user.toString)
-
     var archivedHakuCount: Int      = 0
     var archivedHakukohdeCount: Int = 0
     val archivedHakuOids: Option[Seq[HakuOid]] = HakuDAO.listArchivableHakuOids() match {
       case hakuOids: Seq[HakuOid] =>
-        HakukohdeDAO.listArchivableHakukohdeOidsByHakuOids(hakuOids) match {
-          case hakukohdeOids: Seq[HakukohdeOid] =>
-            logger.info(s"Arkistoidaan julkaistut haut: $hakuOids ja niiden julkaistut hakukohteet: $hakukohdeOids.")
+        if (hakuOids.nonEmpty) {
+          HakukohdeDAO.listArchivableHakukohdeOidsByHakuOids(hakuOids) match {
+            case hakukohdeOids: Seq[HakukohdeOid] =>
+              logger.info(s"Arkistoidaan julkaistut haut: $hakuOids ja niiden julkaistut hakukohteet: $hakukohdeOids.")
 
-            archivedHakukohdeCount = HakukohdeDAO.archiveHakukohdesByHakukohdeOids(hakukohdeOids)
-            hakukohdeOids.map(hakukohdeOid => auditLog.logArchive(hakukohdeOid.toString, AuditResource.Hakukohde, user))
+              archivedHakukohdeCount = HakukohdeDAO.archiveHakukohdesByHakukohdeOids(hakukohdeOids)
+              hakukohdeOids.map(hakukohdeOid =>
+                auditLog.logArchive(hakukohdeOid.toString, AuditResource.Hakukohde, user)
+              )
 
-            archivedHakuCount = HakuDAO.archiveHakusByHakuOids(hakuOids)
-            hakuOids.map(hakuOid => auditLog.logArchive(hakuOid.toString, AuditResource.Haku, user))
+              archivedHakuCount = HakuDAO.archiveHakusByHakuOids(hakuOids)
+              hakuOids.map(hakuOid => auditLog.logArchive(hakuOid.toString, AuditResource.Haku, user))
 
-            Some(hakuOids)
-          case _ =>
-            logger.info(s"Ei löytynyt arkistoitavia hakukohteita hauille: $hakuOids, arkistoidaan haut.")
-            archivedHakuCount = HakuDAO.archiveHakusByHakuOids(hakuOids)
-            Some(hakuOids)
-        }
-      case _ =>
-        logger.info(s"Ei löytynyt yhtään arkistoitavaa hakua, ei arkistoida myöskään yhtään hakukohdetta.")
-        None
+              return Some(hakuOids)
+            case _ =>
+              logger.info(s"Ei löytynyt arkistoitavia hakukohteita hauille: $hakuOids, arkistoidaan haut.")
+              archivedHakuCount = HakuDAO.archiveHakusByHakuOids(hakuOids)
+              return Some(hakuOids)
+          }
+        } else None
     }
     logger.info(s"Arkistointi valmis, arkistoitiin $archivedHakuCount hakua ja $archivedHakukohdeCount hakukohdetta.")
     archivedHakuOids
