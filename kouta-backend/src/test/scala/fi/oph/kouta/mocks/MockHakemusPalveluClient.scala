58d99141de94df323d345794418e2f672be7fe59
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2022-08-17 10:36:38 +0300

    OPHJOD-45: Testataan hakumuspalveluClientiä

--- kouta-backend/src/test/scala/fi/oph/kouta/mocks/MockHakemusPalveluClient.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/mocks/MockHakemusPalveluClient.scala
@@ -5,5 +5,11 @@ import fi.oph.kouta.client.HakemusPalveluClient
 import java.util.UUID
 
 class MockHakemusPalveluClient extends HakemusPalveluClient {
-  override def isExistingAtaruId(ataruId: UUID): Boolean = true
+  override def isExistingAtaruId(ataruId: UUID): Boolean = {
+    if (ataruId == UUID.fromString("c6eead54-a075-44db-b0c5-ba99915958d4")) {
+      false
+    } else {
+      true
+    }
+  }
 }
