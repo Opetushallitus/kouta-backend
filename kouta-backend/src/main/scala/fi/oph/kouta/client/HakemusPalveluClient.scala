b8fcf4637deab6d59c75c243603216f1db059d57
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2022-08-17 15:04:11 +0300

    OPHJOD-45: Lisätään logitusta

--- kouta-backend/src/main/scala/fi/oph/kouta/client/HakemusPalveluClient.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/client/HakemusPalveluClient.scala
@@ -56,7 +56,7 @@ object HakemusPalveluClient extends HakemusPalveluClient with HttpClient with Ca
           .fold(Task.fail, url => {
             client.fetch(Request(method = GET, uri = url)) {
               case r if r.status.code == 200 =>
-                logger.info("repsonse: " + r)
+                logger.info("repsonse: " + r.bodyAsText)
                 r.bodyAsText
                   .runLog
                   .map(_.mkString)
