8179e06cdd2390eeaafe6b18c60b6689e1a71c0c
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-08-29 15:40:01 +0300

    OPHJOD-3: Lisätty toteutukselle laajuusyksikön integriteetin validointi ja parannettu laajuuden validointia sekä testejä

--- kouta-backend/src/test/scala/fi/oph/kouta/util/ToteutusServiceUtilSpec.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/util/ToteutusServiceUtilSpec.scala
@@ -1,35 +1,60 @@
 package fi.oph.kouta.util
 
 class ToteutusServiceUtilSpec extends UnitSpec {
-  "validateOpintojenlaajuus" should "succeed for kk-opintokokonaisuus with laajuus in the defined range" in {
+  "isValidOpintojenLaajuus" should "return true for kk-opintokokonaisuus with laajuus in the defined range" in {
     assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(15), Some(10)) == true)
   }
 
-  "validateOpintojenlaajuus" should "succeed if opintojenlaajuus is the same as min value" in {
+  it should "return true if opintojenlaajuus is the same as min value" in {
     assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(15), Some(5)) == true)
   }
 
-  "validateOpintojenlaajuus" should "succeed if opintojenlaajuus is the same as max value" in {
+  it should "return true if opintojenlaajuus is the same as max value" in {
     assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(15), Some(15)) == true)
   }
 
-  "validateOpintojenlaajuus" should "succeed if opintojenlaajuus is the same as min value and max value is not defined" in {
+  it should "return true if opintojenlaajuus is equal to min value and max value is not defined" in {
     assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), None, Some(5)) == true)
   }
 
-  "validateOpintojenlaajuus" should "succeed if opintojenlaajuus is the same as max value and min value is not defined" in {
-    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(None, Some(5), Some(5)) == true)
+  it should "return true if opintojenlaajuus bigger than min value and max value is not defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), None, Some(6)) == true)
   }
 
-  "validateOpintojenlaajuus" should "fail if opintojenlaajuus is different from min value and max value is not defined" in {
-    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), None, Some(6)) == false)
+  it should "return true if opintojenlaajuus is bigger than min value and max value is not defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), None, Some(6)) == true)
   }
 
-  "validateOpintojenlaajuus" should "fail if opintojenlaajuus is different from max value and min value is not defined" in {
-    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(None, Some(15), Some(6)) == false)
+  it should "return true if opintojenlaajuus is smaller than max value and min value is not defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(None, Some(15), Some(14)) == true)
   }
 
-  "validateOpintojenlaajuus" should "succeed if min and max values are not defined" in {
+  it should "return true if min and max values are not defined" in {
     assert(ToteutusServiceUtil.isValidOpintojenlaajuus(None, None, Some(6)) == true)
   }
+
+  it should "return true if laajuus, min and max are the same" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(5), Some(5)) == true)
+  }
+
+  it should "return true if toteutus laajuus is not defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(15), None) == true)
+  }
+
+  it should "return false if laajuus smaller than min and max also defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(15), Some(4)) == false)
+  }
+
+  it should "return false if laajuus smaller than min and max not defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), None, Some(4)) == false)
+  }
+
+  it should "return false if laajuus bigger than max and min also defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(Some(5), Some(15), Some(16)) == false)
+  }
+
+  it should "return false if laajuus bigger than max and min not defined" in {
+    assert(ToteutusServiceUtil.isValidOpintojenlaajuus(None, Some(15), Some(16)) == false)
+  }
+
 }
