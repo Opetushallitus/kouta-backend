fbbad0e47d85bae35bce1c6622129ac64719eb5a
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-09-05 10:46:28 +0300

    OPHJOD-51: Refaktoroitu koulutus-seach testi käyttämään mockattua KoutasearchClient:a ja siivottu koodia

--- kouta-backend/src/test/scala/fi/oph/kouta/integration/SearchSpec.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/integration/SearchSpec.scala
@@ -1,83 +1,106 @@
 package fi.oph.kouta.integration
 
 import fi.oph.kouta.TestOids._
+import fi.oph.kouta.domain._
 import fi.oph.kouta.domain.oid._
-import fi.oph.kouta.integration.fixture.ElasticDumpFixture
+import fi.oph.kouta.domain.searchResults.{HakukohdeSearchResult, KoulutusSearchResult, ValintaperusteSearchResult}
 import fi.oph.kouta.security.RoleEntity
-import org.mockserver.model
+import fi.oph.kouta.servlet.SearchParams
+import org.json4s.jackson.Serialization.read
+import org.mockito.scalatest.MockitoSugar
 import org.scalatest.BeforeAndAfterEach
+
+import java.time.LocalDateTime
+import java.util.UUID
 
 class SearchSpec
     extends KoutaIntegrationSpec
     with AccessControlSpec
-    with EverythingFixture
     with SearchFixture
-    with ElasticDumpFixture
-    with BeforeAndAfterEach {
+    with BeforeAndAfterEach
+    with MockitoSugar {
 
   override val roleEntities = RoleEntity.all
   override val DebugJson    = false
 
-  var koid1, koid2, koid3, koid4, koid5, koid6: String = _
-  var toid1, toid2, toid3, toid4, toid5, toid6: String = _
-  var hoid1, hoid2, hoid3: String                      = _
-  var hkoid1, hkoid2, hkoid3, hkoid4, hkoid5: String   = _
-  var vpid1, vpid2, vpid3, vpid4, vpid5: String        = _
+  var koulutus1, koulutus2, koulutus3, koulutus4, koulutus5, koulutus6: Koulutus = _
+  var toteutus1, toteutus2, toteutus3, toteutus4, toteutus5, toteutus6: Toteutus = _
+  var hoid1, hoid2, hoid3: String                                                = _
+  var hkoid1, hkoid2, hkoid3, hkoid4, hkoid5: String                             = _
+  var vpid1, vpid2, vpid3, vpid4, vpid5: String                                  = _
+
+  var toteutusOidsByKoulutusOid: Map[KoulutusOid, List[Toteutus]] = Map()
 
   val params = Map("nimi" -> "Hassu", "page" -> "1", "koulutustyyppi" -> "amm")
 
   def barams(organisaatioOid: OrganisaatioOid): Map[String, String] = params + ("organisaatioOid" -> organisaatioOid.s)
   def mockParams(oids: List[String]): Map[String, String] =
-    params + ("oids" -> oids.map(_.toString).sorted.mkString(","))
+    params + ("oids" -> oids.sorted.mkString(","))
   def mockIdParams(ids: List[String]): Map[String, String] =
-    params + ("ids" -> ids.map(_.toString).sorted.mkString(","))
-
-  var mocks: Seq[model.HttpRequest] = Seq()
-
-  def addMock(mockRequest: model.HttpRequest): Unit =
-    mocks = mocks :+ mockRequest
+    params + ("ids" -> ids.sorted.mkString(","))
 
   override def beforeAll(): Unit = {
     super.beforeAll()
     createTestData()
   }
 
-  override def afterEach {
-    super.afterEach()
-    mocks.foreach(clearMock)
-    mocks = Seq()
+  def addKoulutusMock(koulutus: Koulutus, session: UUID): Koulutus = {
+    val oid = put(koulutus, session)
+    koulutus.copy(oid = Some(KoulutusOid(oid)))
+  }
+
+  def addToteutusMock(toteutus: Toteutus): Toteutus = {
+    val oid         = put(toteutus)
+    val koulutusOid = toteutus.koulutusOid
+    val t           = toteutus.copy(oid = Some(ToteutusOid(oid)))
+
+    toteutusOidsByKoulutusOid += (koulutusOid -> (toteutusOidsByKoulutusOid.getOrElse(
+      koulutusOid,
+      List[Toteutus]()
+    ) ++ List(t).distinct))
+    t
   }
 
   def createTestData(): Unit = {
-    koid1 = put(koulutus.copy(organisaatioOid = GrandChildOid, tarjoajat = List(GrandChildOid)), ophSession)
-    koid2 = put(koulutus.copy(organisaatioOid = ParentOid, tarjoajat = List(ParentOid)), ophSession)
-    koid3 = put(koulutus.copy(organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid)), ophSession)
-    koid4 = put(koulutus.copy(organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid), julkinen = true), ophSession)
-    koid5 = put(
+    koulutus1 =
+      addKoulutusMock(koulutus.copy(organisaatioOid = GrandChildOid, tarjoajat = List(GrandChildOid)), ophSession)
+    koulutus2 = addKoulutusMock(koulutus.copy(organisaatioOid = ParentOid, tarjoajat = List(ParentOid)), ophSession)
+    koulutus3 = addKoulutusMock(koulutus.copy(organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid)), ophSession)
+    koulutus4 = addKoulutusMock(
+      koulutus.copy(organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid), julkinen = true),
+      ophSession
+    )
+    koulutus5 = addKoulutusMock(
       koulutus.copy(organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid, ChildOid), julkinen = true),
       ophSession
     )
-    koid6 = put(koulutus.copy(organisaatioOid = OphOid, tarjoajat = List(LonelyOid), julkinen = true), ophSession)
+    koulutus6 =
+      addKoulutusMock(koulutus.copy(organisaatioOid = OphOid, tarjoajat = List(LonelyOid), julkinen = true), ophSession)
 
-    toid1 = put(
-      toteutus.copy(koulutusOid = KoulutusOid(koid1), organisaatioOid = GrandChildOid, tarjoajat = List(GrandChildOid))
+    toteutus1 = addToteutusMock(
+      toteutus.copy(koulutusOid = koulutus1.oid.get, organisaatioOid = GrandChildOid, tarjoajat = List(GrandChildOid))
     )
-    toid2 = put(
-      toteutus.copy(koulutusOid = KoulutusOid(koid2), organisaatioOid = ParentOid, tarjoajat = List(ParentOid))
-    )
-    toid3 = put(
-      toteutus.copy(koulutusOid = KoulutusOid(koid3), organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid))
-    )
-    toid4 = put(
-      toteutus.copy(koulutusOid = KoulutusOid(koid4), organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid))
-    )
-    toid5 = put(
-      toteutus.copy(koulutusOid = KoulutusOid(koid5), organisaatioOid = GrandChildOid, tarjoajat = List(GrandChildOid))
-    )
-    toid6 = put(
-      toteutus.copy(koulutusOid = KoulutusOid(koid6), organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid))
+    toteutus2 = addToteutusMock(
+      toteutus.copy(koulutusOid = koulutus2.oid.get, organisaatioOid = ParentOid, tarjoajat = List(ParentOid))
     )
 
+    toteutus3 = addToteutusMock(
+      toteutus.copy(koulutusOid = koulutus3.oid.get, organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid))
+    )
+
+    toteutus4 = addToteutusMock(
+      toteutus.copy(koulutusOid = koulutus4.oid.get, organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid))
+    )
+
+    toteutus5 = addToteutusMock(
+      toteutus.copy(koulutusOid = koulutus5.oid.get, organisaatioOid = GrandChildOid, tarjoajat = List(GrandChildOid))
+    )
+
+    toteutus6 = addToteutusMock(
+      toteutus.copy(koulutusOid = koulutus6.oid.get, organisaatioOid = LonelyOid, tarjoajat = List(LonelyOid))
+    )
+
+    /*
     hoid1 = put(haku.copy(organisaatioOid = ParentOid))
     hoid2 = put(haku.copy(organisaatioOid = LonelyOid))
     hoid3 = put(haku.copy(organisaatioOid = OphOid))
@@ -87,38 +110,87 @@ class SearchSpec
     hkoid3 = put(hakukohdeWoValintaperusteenValintakokeet.copy(toteutusOid = ToteutusOid(toid3), hakuOid = HakuOid(hoid2), organisaatioOid = LonelyOid))
     hkoid4 = put(hakukohdeWoValintaperusteenValintakokeet.copy(toteutusOid = ToteutusOid(toid3), hakuOid = HakuOid(hoid3), organisaatioOid = LonelyOid))
     hkoid5 = put(hakukohdeWoValintaperusteenValintakokeet.copy(toteutusOid = ToteutusOid(toid5), hakuOid = HakuOid(hoid3), organisaatioOid = GrandChildOid))
-
+    
     vpid1 = put(valintaperuste.copy(organisaatioOid = GrandChildOid, julkinen = false)).toString
     vpid2 = put(valintaperuste.copy(organisaatioOid = ParentOid, julkinen = false)).toString
     vpid3 = put(valintaperuste.copy(organisaatioOid = OphOid, julkinen = false)).toString
     vpid4 = put(valintaperuste.copy(organisaatioOid = LonelyOid, julkinen = false)).toString
     vpid5 = put(valintaperuste.copy(organisaatioOid = LonelyOid, julkinen = true)).toString
+    */
+
   }
 
-  /*
-  "Search koulutukset" should "search allowed koulutukset and allowed toteutus counts 1" in {
-    addMock(mockKoulutusResponse(
-      List(koid1, koid2, koid4, koid5, koid6),
-      params,
-      List(
-        KoulutusResponseData(oid = koid1, organisaatiot = List(GrandChildOid.toString())),
-        KoulutusResponseData(oid = koid2, organisaatiot = List(ParentOid.toString())),
-        KoulutusResponseData(oid = koid4, organisaatiot = List(LonelyOid.toString())), // NOTE: tämä on ainoa jolle ei pitäisi löytyä
-        KoulutusResponseData(oid = koid5, organisaatiot = List(LonelyOid.toString(), ChildOid.toString())),
-        KoulutusResponseData(oid = koid6, organisaatiot = List(LonelyOid.toString())),
-      )))
+  "Koulutus search" should "search allowed koulutukset and allowed toteutus counts 1" in {
+    // TODO: Tämä kannattaa ehkä muuttaa siten, että mockkaa elastic-kyselyn vastauksen.
+    // Koulutukset ja toteutusten organisaatiot, jotka KoutaSearchClientin pitäisi palauttaa tämän testin kyselylle
+    val koulutusJaOrganisaatiot = List(
+      (koulutus1, List(GrandChildOid)),
+      (koulutus2, List(ParentOid)),
+      (koulutus4, List(LonelyOid)),
+      (koulutus5, List(LonelyOid, ChildOid)),
+      (koulutus6, List(LonelyOid))
+    )
+    when(
+      mockKoutaSearchClient.searchKoulutukset(
+        koulutusJaOrganisaatiot.map(_._1.oid.get),
+        SearchParams(params)
+      )
+    ).thenAnswer(
+      SearchResult[KoulutusSearchItemFromIndex](
+        totalCount = koulutusJaOrganisaatiot.length,
+        result = koulutusJaOrganisaatiot.map(kData => {
+          val k             = kData._1
+          val organisaatiot = kData._2
+          KoulutusSearchItemFromIndex(
+            oid = k.oid.get,
+            nimi = k.nimi,
+            koulutustyyppi = k.koulutustyyppi,
+            organisaatio = Organisaatio(
+              oid = k.organisaatioOid,
+              nimi = Map(Fi -> "fi"),
+              paikkakunta = Paikkakunta(
+                nimi = Map(Fi -> "fi"),
+                koodiUri = "paikkakunta_1#1"
+              )
+            ),
+            muokkaaja = Muokkaaja(nimi = "Matti Muokkaaja", oid = k.muokkaaja),
+            modified = k.modified.getOrElse(Modified(LocalDateTime.now())),
+            tila = k.tila,
+            toteutukset = toteutusOidsByKoulutusOid
+              .getOrElse(k.oid.get, List())
+              .map(t =>
+                KoulutusSearchItemToteutus(
+                  t.oid.get,
+                  nimi = t.nimi,
+                  organisaatio = Organisaatio(
+                    oid = t.organisaatioOid,
+                    nimi = Map(Fi -> "fi"),
+                    paikkakunta = Paikkakunta(
+                      nimi = Map(Fi -> "fi"),
+                      koodiUri = "paikkakunta_1#1"
+                    )
+                  ),
+                  modified = t.modified.getOrElse(Modified(LocalDateTime.now())),
+                  tila = t.tila,
+                  organisaatiot = organisaatiot.map(_.toString)
+                )
+              )
+          )
+        })
+      )
+    )
 
     get(s"$SearchPath/koulutukset", barams(ChildOid), Seq(sessionHeader(readSessions(ChildOid)))) {
-      status should equal (200)
+      status should equal(200)
       debugJson[KoulutusSearchResult](body)
       val r = read[KoulutusSearchResult](body).result
-      r.map(_.oid.s) should be (List(koid1, koid2, koid4, koid5, koid6))
-      r.map(_.toteutusCount) should be (List(1, 1, 0, 1, 0))
+      r.map(_.oid.s) should be(koulutusJaOrganisaatiot.map(_._1.oid.get.s))
+      r.map(_.toteutusCount) should be(List(1, 1, 0, 1, 0))
     }
   }
-   */
 
-  /* TODO: These should be converted (as in above test) to use mocked organisaatio lists
+  //TODO: These should be converted (as in above test) to use mocked organisaatio lists
+  /*
   it should "search allowed koulutukset and allowed toteutus counts 2" in {
     addMock(mockKoulutusResponse(List(koid3, koid4, koid5, koid6), params, List(koid3, koid4, koid5, koid6)))
 
@@ -153,30 +225,29 @@ class SearchSpec
     }
   } */
 
-  /*
-  it should "return empty result if there are no allowed koulutukset" in {
+  "Koulutus search" should "return empty result if there are no allowed koulutukset" in {
     get(s"$SearchPath/koulutukset", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
-      status should equal (200)
-      read[KoulutusSearchResult](body).result.size should be (0)
+      status should equal(200)
+      read[KoulutusSearchResult](body).result.size should be(0)
     }
   }
 
-  it should "return 403 if organisaatio is not allowed" in {
+  "Koulutus search" should "return 403 if organisaatio is not allowed" in {
     get(s"$SearchPath/koulutukset", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
-      status should equal (403)
+      status should equal(403)
     }
   }
 
-  it should "return 500 if kouta index returns 500" in {
+  /*
+  it should "return 500 if elasticsearch returns 500" in {
     addMock(mockKoulutusResponse(List(koid3, koid4, koid5, koid6), params, List(), 500))
 
     get(s"$SearchPath/koulutukset", barams(LonelyOid), Seq(sessionHeader(crudSessions(LonelyOid)))) {
       status should equal (500)
     }
   }
-   */
 
-  /* TODO: update mockToteutusResponse
+  // TODO: update mockToteutusResponse
   "Search toteutukset" should "search allowed toteutukset and allowed hakukohde counts 1" in {
     addMock(mockToteutusResponse(List(toid1, toid2, toid5), params, List(toid1, toid2, toid5)))
 
@@ -209,22 +280,21 @@ class SearchSpec
       debugJson[ToteutusSearchResult](body)
       read[ToteutusSearchResult](body).result.size should be (0)
     }
-  } */
-
-  /*
-  "Search toteutukset" should "return empty result if there are no allowed toteutukset" in {
-    get(s"$SearchPath/toteutukset", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
-      status should equal (200)
-      read[ToteutusSearchResult](body).result.size should be (0)
-    }
-  }
-
-  it should "return 403 if organisaatio is not allowed" in {
-    get(s"$SearchPath/toteutukset", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
-      status should equal (403)
-    }
   }
    */
+
+  "Toteutus search" should "return empty result if there are no allowed toteutukset" in {
+    get(s"$SearchPath/toteutukset", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
+      status should equal(200)
+      read[ToteutusSearchResult](body).result.size should be(0)
+    }
+  }
+
+  "Toteutus search" should "return 403 if organisaatio is not allowed" in {
+    get(s"$SearchPath/toteutukset", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
+      status should equal(403)
+    }
+  }
 
   /*
   it should "return 500 if kouta index returns 500" in {
@@ -268,9 +338,9 @@ class SearchSpec
       debugJson[HakuSearchResult](body)
       read[HakuSearchResult](body).result.size should be (0)
     }
-  } */
+  }
+ */
 
-  /*
   ignore should "return empty result if there are no allowed haut" in { //TODO: koulutustyyppi haulle?
     get(s"$SearchPath/haut", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
       status should equal (200)
@@ -278,12 +348,11 @@ class SearchSpec
     }
   }
 
-  "Search haut" should "return 403 if organisaatio is not allowed" in {
+  "Haku search" should "return 403 if organisaatio is not allowed" in {
     get(s"$SearchPath/haut", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
       status should equal (403)
     }
   }
-   */
 
   /*
   it should "return 500 if kouta index returns 500" in {
@@ -326,19 +395,23 @@ class SearchSpec
       read[HakukohdeSearchResult](body).result.size should be (0)
     }
   }
+  */
 
-  it should "return empty result if there are no allowed hakukohteet" in {
+
+  "Hakukohde search" should "return empty result if there are no allowed hakukohteet" in {
     get(s"$SearchPath/hakukohteet", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
       status should equal (200)
       read[HakukohdeSearchResult](body).result.size should be (0)
     }
   }
 
-  it should "return 403 if organisaatio is not allowed" in {
+  "Hakukohde search" should "return 403 if organisaatio is not allowed" in {
     get(s"$SearchPath/hakukohteet", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
       status should equal (403)
     }
   }
+
+  /*
 
   it should "return 500 if kouta index returns 500" in {
     addMock(mockHakukohdeResponse(List(hkoid1, hkoid5), params, List(), 500))
@@ -379,19 +452,22 @@ class SearchSpec
       read[HakukohdeSearchResult](body).result.size should be (0)
     }
   }
+  */
 
-  it should "return empty result if there are no allowed valintaperusteet" in {
-    get(s"$SearchPath/valintaperusteet", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
-      status should equal (200)
-      read[ValintaperusteSearchResult](body).result.size should be (0)
+    "Valintaperuste search" should "return empty result if there are no allowed valintaperusteet"  in {
+      get(s"$SearchPath/valintaperusteet", barams(YoOid), Seq(sessionHeader(readSessions(YoOid)))) {
+        status should equal(200)
+        read[ValintaperusteSearchResult](body).result.size should be(0)
+      }
     }
-  }
 
-  it should "return 403 if organisaatio is not allowed" in {
-    get(s"$SearchPath/valintaperusteet", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
-      status should equal (403)
+
+    "Valintaperuste search" should "return 403 if organisaatio is not allowed" in {
+      get(s"$SearchPath/valintaperusteet", barams(ChildOid), Seq(sessionHeader(readSessions(YoOid)))) {
+        status should equal(403)
+      }
     }
-  }
+  /*
 
   it should "return 500 if kouta index returns 500" in {
     addMock(mockValintaperusteResponse(List(vpid3, vpid4, vpid5), params, List(), 500))
@@ -401,4 +477,5 @@ class SearchSpec
     }
   }
    */
+
 }
