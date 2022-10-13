5458b7ed304d750e270d8a1bbae686509d0a2646
August Kilponen <august.kilponen@gmail.com>
2022-10-13 16:03:02 +0300

    Estetty Koutan etusivulta hakukohteen lisäämisen yrittäminen sellaiselle hakukohteelle joka ei käytä atarun lomaketta

--- kouta-backend/src/main/scala/fi/oph/kouta/repository/toteutusDAO.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/repository/toteutusDAO.scala
@@ -2,7 +2,7 @@ package fi.oph.kouta.repository
 
 import java.time.Instant
 import fi.oph.kouta.domain.oid._
-import fi.oph.kouta.domain.{AmmOsaamisala, AmmTutkinnonOsa, Ataru, OidAndNimi, TilaFilter, Toteutus, ToteutusEnrichedData, ToteutusListItem, VapaaSivistystyoMuu}
+import fi.oph.kouta.domain.{AikuistenPerusopetus, AmmOsaamisala, AmmTutkinnonOsa, Ataru, OidAndNimi, TilaFilter, Toteutus, ToteutusEnrichedData, ToteutusListItem, VapaaSivistystyoMuu}
 import fi.oph.kouta.service.ToteutusService
 import fi.oph.kouta.util.MiscUtils.optionWhen
 import fi.oph.kouta.util.TimeUtils.instantToModified
@@ -360,6 +360,7 @@ sealed trait ToteutusSQL extends ToteutusExtractors with ToteutusModificationSQL
                  or tt.tarjoaja_oid in (#${createOidInParams(organisaatioOids)}))
                 and (((t.metadata->>'tyyppi')::koulutustyyppi is distinct from ${AmmTutkinnonOsa.toString}::koulutustyyppi
                        and (t.metadata->>'tyyppi')::koulutustyyppi is distinct from ${AmmOsaamisala.toString}::koulutustyyppi
+                       and (t.metadata->>'tyyppi')::koulutustyyppi is distinct from ${AikuistenPerusopetus.toString}::koulutustyyppi
                        and (t.metadata->>'tyyppi')::koulutustyyppi is distinct from ${VapaaSivistystyoMuu.toString}::koulutustyyppi )
                      or (t.metadata->>'hakulomaketyyppi')::hakulomaketyyppi = ${Ataru.toString}::hakulomaketyyppi )
                     #${tilaConditions(tilaFilter, "t.tila")}""".as[ToteutusListItem]
