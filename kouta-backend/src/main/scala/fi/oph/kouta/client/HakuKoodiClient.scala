4fb67e35e97d5357419ca3911c611c4d4ee7068f
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-01-26 16:02:15 +0200

    Poistettu HakukoodiConstants

--- kouta-backend/src/main/scala/fi/oph/kouta/client/HakuKoodiClient.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/client/HakuKoodiClient.scala
@@ -5,9 +5,4 @@ import fi.vm.sade.properties.OphProperties
 
 object HakuKoodiClient extends HakuKoodiClient(KoutaConfigurationFactory.configuration.urlProperties)
 
-package object HakukoodiConstants {
-  val hakukohdeKoodistoAmmErityisopetus = "hakukohteeterammatillinenerityisopetus"
-  val hakukohdeKoodistoPoJalkYhteishaku = "hakukohteetperusopetuksenjalkeinenyhteishaku"
-}
-
 class HakuKoodiClient(urlProperties: OphProperties) extends CachedKoodistoClient(urlProperties)
