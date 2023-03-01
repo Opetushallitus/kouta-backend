ee6a348310e005cfeb2d379f616aeffb85c60527
August Kilponen <august.kilponen@gmail.com>
2023-03-01 17:19:56 +0200

    Lisätty rajapinta hakukohteen tietojen hakemiseksi hakemuspalvelusta

--- kouta-backend/src/main/scala/fi/oph/kouta/ScalatraBootstrap.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/ScalatraBootstrap.scala
@@ -37,6 +37,7 @@ class ScalatraBootstrap extends LifeCycle with Logging {
     context.mount(new ExternalServlet(), "/external", "external")
     context.mount(new ArkistointiServlet(), "/archiver","archiver")
     context.mount(new OrganisaatioServlet(), "/organisaatio", "organisaatio")
+    context.mount(new HakemuspalveluServlet(), "/hakemuspalvelu", "hakemuspalvelu")
     context.mount(new SwaggerServlet, "/swagger")
 
     val schedulerConfig = new SchedulerConfig
