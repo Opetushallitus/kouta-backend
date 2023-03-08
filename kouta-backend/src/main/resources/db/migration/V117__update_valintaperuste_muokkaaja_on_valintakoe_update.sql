27763256d546547564c9d7a15a81110d43165a1f
Marja Kari <marja.kari@amban.fi>
2023-03-08 13:19:52 +0200

    OY-4121 hakukohteiden muokkaajan päivitys triggerifunktiolla

--- kouta-backend/src/main/resources/db/migration/V117__update_valintaperuste_muokkaaja_on_valintakoe_update.sql
+++ kouta-backend/src/main/resources/db/migration/V117__update_valintaperuste_muokkaaja_on_valintakoe_update.sql
@@ -1,20 +1,20 @@
--- -- Päivitetään valintaperusteen last_modified ja muokkaaja kun sen valintakokeet muuttuu, mutta vain jos last_modified muuttuisi.
--- -- Näin vältetään turhat muutokset valintaperusteet-tauluun, kun useampi valintakoe muuttuu samalla
--- create or replace function set_valintaperusteet_last_modified_from_related() returns trigger as
--- $$
--- begin
---     if (tg_op = 'DELETE') then
---         update valintaperusteet
---         set last_modified = now()::timestamptz, muokkaaja = old.muokkaaja
---         where id = old.valintaperuste_id
---           and last_modified <> now()::timestamptz;
---         return null;
---     else
---         update valintaperusteet
---         set last_modified = now()::timestamptz, muokkaaja = new.muokkaaja
---         where id = new.valintaperuste_id
---           and last_modified <> now()::timestamptz;
---         return null;
---     end if;
--- end;
--- $$ language plpgsql;
+-- Päivitetään valintaperusteen last_modified ja muokkaaja kun sen valintakokeet muuttuu, mutta vain jos last_modified muuttuisi.
+-- Näin vältetään turhat muutokset valintaperusteet-tauluun, kun useampi valintakoe muuttuu samalla
+create or replace function set_valintaperusteet_last_modified_from_related() returns trigger as
+$$
+begin
+    if (tg_op = 'DELETE') then
+        update valintaperusteet
+        set last_modified = now()::timestamptz, muokkaaja = old.muokkaaja
+        where id = old.valintaperuste_id
+          and last_modified <> now()::timestamptz;
+        return null;
+    else
+        update valintaperusteet
+        set last_modified = now()::timestamptz, muokkaaja = new.muokkaaja
+        where id = new.valintaperuste_id
+          and last_modified <> now()::timestamptz;
+        return null;
+    end if;
+end;
+$$ language plpgsql;
