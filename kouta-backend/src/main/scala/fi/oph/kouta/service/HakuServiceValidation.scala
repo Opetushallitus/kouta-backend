bb9e6301dbf54a6186459ca82f0549572b72794b
Dmitry Marokhonov <dmitry.marokhonov@knowit.fi>
2023-12-15 23:24:40 +0200

    OY-4340: Validation hakukohteenliittajaorganisaatiot revert previous changes

--- kouta-backend/src/main/scala/fi/oph/kouta/service/HakuServiceValidation.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/service/HakuServiceValidation.scala
@@ -2,7 +2,7 @@ package fi.oph.kouta.service
 
 import fi.oph.kouta.client.HakemusPalveluClient
 import fi.oph.kouta.domain._
-import fi.oph.kouta.domain.oid.{OrganisaatioOid, RootOrganisaatioOid}
+import fi.oph.kouta.domain.oid.OrganisaatioOid
 import fi.oph.kouta.repository.HakukohdeDAO
 import fi.oph.kouta.validation.CrudOperations.{create, update}
 import fi.oph.kouta.validation.Validations._
@@ -125,7 +125,7 @@ class HakuServiceValidation(
   }
 
   private def validateHakukohteenLiittajaOrganisaatiot(liittajat: Seq[OrganisaatioOid]): IsValid = {
-    val allOids = organisaatioService.getAllChildOidsFlat(RootOrganisaatioOid)
+    val allOids = organisaatioService.getAllOrganisaatioOids()
     assertTrue(if (liittajat.isEmpty) true else liittajat.forall(
         allOids.contains
       ),
