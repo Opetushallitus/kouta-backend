4f69deaa75dc8e82c58ed3eef9ff93e1623fdea2
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-01-09 13:27:04 +0200

    OK-410: Lisätty aloituspaikat ja ensisijaisestiHakeneet pistehistoriaan

--- kouta-backend/src/test/scala/fi/oph/kouta/mocks/MockHakemusPalveluClient.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/mocks/MockHakemusPalveluClient.scala
@@ -1,14 +1,21 @@
 package fi.oph.kouta.mocks
 
 import fi.oph.kouta.client.{HakemusPalveluClient, HakukohdeInfo}
-import fi.oph.kouta.domain.oid.HakukohdeOid
+import fi.oph.kouta.domain.oid.{HakuOid, HakukohdeOid}
 import fi.oph.kouta.validation.ExternalQueryResults.{ExternalQueryResult, itemFound, itemNotFound}
 
 import java.util.UUID
 
-class MockHakemusPalveluClient extends HakemusPalveluClient {
+class MockHakemusPalveluClient() extends HakemusPalveluClient {
+  var ensisijainenApplicationCounts: HakukohdeApplicationCounts = Map()
+
   override def isExistingAtaruIdFromCache(ataruId: UUID): ExternalQueryResult = itemFound
   override def isFormAllowedForHakutapa(ataruId: UUID, hakutapaKoodiUri: Option[String]): ExternalQueryResult = itemNotFound
 
   override def getHakukohdeInfo(hakukohdeOid: HakukohdeOid): HakukohdeInfo = HakukohdeInfo(applicationCount = 0)
+
+  override def getEnsisijainenApplicationCounts(
+      hakuOid: HakuOid
+  ): HakukohdeApplicationCounts =
+    ensisijainenApplicationCounts
 }
