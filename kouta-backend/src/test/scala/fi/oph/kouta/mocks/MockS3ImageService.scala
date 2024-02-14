62c814f0d8050d823ba5ced28f998b6771fb823f
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2024-02-14 10:29:48 +0200

    OK-412: Lisätty logiikka tietojen hakuun / tallennukseen palasissa

--- kouta-backend/src/test/scala/fi/oph/kouta/mocks/MockS3ImageService.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/mocks/MockS3ImageService.scala
@@ -7,5 +7,5 @@ import fi.oph.kouta.integration.fixture.MockS3Client
 
 object MockS3ImageService extends S3ImageService(MockS3Client, new AuditLog(MockAuditLogger)) {
   override lazy val config: S3Configuration =
-    S3Configuration("konfo-files", "https://konfo-files.untuvaopintopolku.fi", "opintopolku-siirtotiedostot", None, 3)
+    S3Configuration("konfo-files", "https://konfo-files.untuvaopintopolku.fi", "opintopolku-siirtotiedostot", None, 3, 10000)
 }
