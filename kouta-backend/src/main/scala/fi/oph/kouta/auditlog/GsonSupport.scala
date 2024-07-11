5ddc3001d28907a6b2949767f85de0267f09a132
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2024-07-11 08:45:47 +0300

    OK-471: Päivitetään Gsonin JsonParserin käyttö (deprekoitunut)

--- kouta-backend/src/main/scala/fi/oph/kouta/auditlog/GsonSupport.scala
+++ kouta-backend/src/main/scala/fi/oph/kouta/auditlog/GsonSupport.scala
@@ -6,11 +6,9 @@ import org.json4s.jackson.Serialization.write
 
 trait GsonSupport extends KoutaJsonFormats {
 
-  private val jsonParser = new JsonParser
-
   def toGson[T <: AnyRef](entity: T): JsonElement = {
     val json = write[T](entity)
 
-    jsonParser.parse(json)
+    JsonParser.parseString(json)
   }
 }
