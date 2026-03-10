2562f075128813783cf5b57fdb59b8eab73af1fc
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-03-10 13:23:33 +0200

    OY-5501: Formatointikorjauksia

--- kouta-backend/src/test/scala/fi/oph/kouta/integration/ToteutusServiceValidationSpec.scala
+++ kouta-backend/src/test/scala/fi/oph/kouta/integration/ToteutusServiceValidationSpec.scala
@@ -103,7 +103,7 @@ class ToteutusServiceValidationSpec extends BaseServiceValidationSpec[Toteutus] 
     InetAddress.getByName("127.0.0.1")
   )
   val nimiNotMatchingDefault = Map(Fi -> "eri nimi", Sv -> "eri nimi sv")
-  val pastMillis = System.currentTimeMillis() - (1000L * 60 * 60 * 24 * 30)
+  val pastMillis             = System.currentTimeMillis() - (1000L * 60 * 60 * 24 * 30)
 
   private def ammToteutusWithOpetusParameters(
       opetuskieliKoodiUrit: Seq[String] = Seq("oppilaitoksenopetuskieli_1#1"),
@@ -397,7 +397,9 @@ class ToteutusServiceValidationSpec extends BaseServiceValidationSpec[Toteutus] 
     val futureMillis = System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 30)
     when(
       ePerusteKoodiClient.getOsaamismerkkiFromEPerusteCache("osaamismerkit_1082")
-    ).thenAnswer(Right(Osaamismerkki(tila = "JULKAISTU", koodiUri = "osaamismerkit_1082", voimassaoloLoppuu = Some(futureMillis))))
+    ).thenAnswer(
+      Right(Osaamismerkki(tila = "JULKAISTU", koodiUri = "osaamismerkit_1082", voimassaoloLoppuu = Some(futureMillis)))
+    )
   }
 
   private def failSorakuvausValidation(toteutus: Toteutus): Assertion = {
@@ -1900,7 +1902,9 @@ class ToteutusServiceValidationSpec extends BaseServiceValidationSpec[Toteutus] 
   it should "fail if the attached osaamismerkki is deprecated (voimassaolo on päättynyt)" in {
     when(
       ePerusteKoodiClient.getOsaamismerkkiFromEPerusteCache("osaamismerkit_1082")
-    ).thenAnswer(Right(Osaamismerkki(tila = "JULKAISTU", koodiUri = "osaamismerkit_1082", voimassaoloLoppuu = Some(pastMillis))))
+    ).thenAnswer(
+      Right(Osaamismerkki(tila = "JULKAISTU", koodiUri = "osaamismerkit_1082", voimassaoloLoppuu = Some(pastMillis)))
+    )
 
     val osaamismerkkiKoulutus1 = VapaaSivistystyoOsaamismerkkiKoulutusEntityListItem.copy(oid = koulutusOid1)
     when(koulutusDao.get(List(koulutusOid1)))
@@ -1928,14 +1932,20 @@ class ToteutusServiceValidationSpec extends BaseServiceValidationSpec[Toteutus] 
     val julkaistuKkOpintokokonaisuusToteutus = kkOpintokokonaisuusToteutus.copy(
       oid = Some(toteutusOid2),
       metadata = Some(KkOpintokokonaisuusToteutuksenMetatieto.copy(liitetytOpintojaksot = List(toteutusOid))),
-      modified = Some(Modified(LocalDateTime.now().minusDays(1))))
+      modified = Some(Modified(LocalDateTime.now().minusDays(1)))
+    )
     when(toteutusDao.get(toteutusOid))
       .thenAnswer(Vector(MinimalExistingToteutus(julkaistuKkOpintokokonaisuusToteutus)))
 
     failsLiitetytValidation(
       julkaistuKkOpintojaksoToteutus.copy(tila = Tallennettu),
       julkaistuKkOpintojaksoToteutus,
-      Seq(ValidationError("metadata.tila", invalidStateChangeForLiitetty(toteutusOid, List(Some(toteutusOid2)), Vector(toteutusOid2))))
+      Seq(
+        ValidationError(
+          "metadata.tila",
+          invalidStateChangeForLiitetty(toteutusOid, List(Some(toteutusOid2)), Vector(toteutusOid2))
+        )
+      )
     )
   }
 
@@ -1965,10 +1975,16 @@ class ToteutusServiceValidationSpec extends BaseServiceValidationSpec[Toteutus] 
   it should "succeed to arkistoida toteutus when liitetty osaamismerkki is deprecated" in {
     when(
       ePerusteKoodiClient.getOsaamismerkkiFromEPerusteCache("osaamismerkit_1083")
-    ).thenAnswer(Right(Osaamismerkki(tila = "JULKAISTU", koodiUri = "osaamismerkit_1082", voimassaoloLoppuu = Some(pastMillis))))
+    ).thenAnswer(
+      Right(Osaamismerkki(tila = "JULKAISTU", koodiUri = "osaamismerkit_1082", voimassaoloLoppuu = Some(pastMillis)))
+    )
     val osaamismerkkiKoulutus1 = VapaaSivistystyoOsaamismerkkiKoulutusEntityListItem.copy(oid = koulutusOid1)
     val osaamismerkkiKoulutus2 =
-      VapaaSivistystyoOsaamismerkkiKoulutusEntityListItem.copy(oid = koulutusOid2, tila = Tallennettu, osaamismerkkiKoodiUri = Some("osaamismerkit_1083#1"))
+      VapaaSivistystyoOsaamismerkkiKoulutusEntityListItem.copy(
+        oid = koulutusOid2,
+        tila = Tallennettu,
+        osaamismerkkiKoodiUri = Some("osaamismerkit_1083#1")
+      )
     when(koulutusDao.get(List(koulutusOid1, koulutusOid2)))
       .thenAnswer(
         Seq(osaamismerkkiKoulutus1, osaamismerkkiKoulutus2)
@@ -1987,7 +2003,6 @@ class ToteutusServiceValidationSpec extends BaseServiceValidationSpec[Toteutus] 
       ) == vstToteutus
     )
   }
-
 
   "Muu validation" should "fail if ammattinimikkeet given" in {
     failsValidation(
