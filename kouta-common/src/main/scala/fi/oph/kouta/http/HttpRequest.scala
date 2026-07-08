0231f41372564529c8432a3ad01be4b373e4ecdc
Petteri Tolonen <petteri.tolonen@knowit.fi>
2026-07-08 09:36:14 +0300

    Poistettu käyttämätöntä koodia

--- kouta-common/src/main/scala/fi/oph/kouta/http/HttpRequest.scala
+++ kouta-common/src/main/scala/fi/oph/kouta/http/HttpRequest.scala
@@ -6,7 +6,6 @@ import scala.collection.immutable.HashMap
 import scalaj.http.HttpResponse
 
 trait HttpRequest{
-  def responseWithHeaders(): (Int, Map[String, Seq[String]], String)
   def responseWithStatus: (Int, String)
   def response(): Option[String]
   def param(key: String, value: String): HttpRequest
@@ -21,17 +20,6 @@ class DefaultHttpRequest(private val request: scalaj.http.HttpRequest) extends H
 
   def header(key: String, value: String) = {
     new DefaultHttpRequest(request.header(key, value))
-  }
-
-  def responseWithHeaders(): (Int, Map[String, Seq[String]], String) = {
-    try {
-      val response: HttpResponse[String] = request.asString
-      (response.code, response.headers, response.body)
-    } catch {
-      case t: Throwable =>
-        logUnexpectedError(t)
-        (500, HashMap(), t.toString)
-    }
   }
 
   override def responseWithStatus: (Int, String) = {
